6542c25e05ffa10807043187db132cae3e432083
MaxTheFirst <maks.vershinin.2005@mail.ru>
2024-12-18 09:37:44 +0300

    last

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from window import window
 import callbacks
-
-from calcs import DeterminantCalc
-from latex import LatexGenerator
-
-import tkinter as tk
-
-def main():
-    array22 = [[[1, (-2, 5)], [3, 4]], [[1, 2], [3, 4]]]
-    array33 = [[1, -2, 3], [4, 5, 6], [7, 8, 9]]
-
-    print(LatexGenerator.get_inverse_matrix_code(array33, array22, array33))
     
 if __name__ == "__main__":
-    main()
+    window.mainloop()
